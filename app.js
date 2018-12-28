40ec9f5cc151f0491eada687af45779dd43bccdf
Seema95 <seema.deshpande@synerzip.com>
2018-12-28 15:50:11 +0530

    Home User Design Ready

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var express = require('express');
 var path = require('path');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-
+//var userHome= require('./routes/HomePageRoute');
 var app = express();
 
 // view engine setup
@@ -16,6 +16,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/home',indexRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,9 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+/* user page*/
+
+router.get('/home', function(req, res, next) {
+  res.render('UserHomePage', { title: 'Express' });
+});
 module.exports = router;
