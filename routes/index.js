1987e65108cf5c470d5c502b108abd7a68b64921
sourabh-karmarkar <sourabh.karmarkar@synerzip.com>
2018-12-28 18:27:07 +0530

    Sign Up Functionality

--- routes/index.js
+++ routes/index.js
@@ -1,22 +1,34 @@
 var express = require('express');
 var router = express.Router();
-var fetchSubject=require('../Database');
+var fetchResults=require('../Database');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('login', { title: 'Express' });
 });
-router.get('/signup.ejs', function(req, res, next) {
-  res.render('signup', { title: 'Express' });
+router.get('/signup', function(req, res, next) {
+  res.render('signup');
 });
 router.get('/login.ejs', function(req, res, next) {
   res.render('login', { title: 'Express' });
 });
 
-
+router.post('/users/add', function(req, res, next) {
+  var newuser={
+    user_name:req.body.name,
+    user_email:req.body.email,
+    user_password:req.body.password,
+    user_phone:req.body.phone_number,
+    user_status:1
+  };
+  fetchResults.insertUser(newuser,(result)=>{
+    console.log("Inserted Succesfully!");
+    res.render('signup');
+  });
+});
 
 router.get('/adminhomepage', function(req, res, next) {
-    fetchSubject.getSubject((results)=>{
+  fetchResults.getSubject((results)=>{
       res.render('AdminHomePage', { title: 'This is Admin Home Page',result:results });
     });
 });
