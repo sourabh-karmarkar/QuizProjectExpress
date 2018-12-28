af674cd99707a1565f3bfbc4568d9a24d6f4b287
Neha Shende <neha.shende@synerzip.com>
2018-12-28 13:24:53 +0530

    Basic database skeleton

---
+++ Database.js
@@ -0,0 +1,19 @@
+var mongo = require('mongodb').MongoClient;
+
+var url = "mongodb://localhost:27017/"
+const dbs = db.db(QuizExpressDB);
+
+MongoClient.connect(url, { useNewUrlParser: true }, function (err, db) {
+    if (err) throw err;
+
+    
+
+    let insertQuestion = insertDocuments(dbs, function () {
+        client.close();
+    });
+    let fetchQuestion = fetchDocs(dbs, function () {
+        client.close();
+    });
+
+
+});
